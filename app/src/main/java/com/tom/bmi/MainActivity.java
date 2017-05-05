3eb7460553effd1fca453fcd5bb18b220b58121b
klin1007 <klin1007@hotmail.com>
2017-05-05 14:10:14 +0800

    改寫MainActivity.java

--- app/src/main/java/com/tom/bmi/MainActivity.java
+++ app/src/main/java/com/tom/bmi/MainActivity.java
@@ -1,9 +1,10 @@
 package com.tom.bmi;
 
+
 import android.app.AlertDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText edWeight;
     private EditText edHeight;
     private Button bHelp;
+    private Button bCalculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,41 +24,73 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
         findViews();
+//        bHelp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                new AlertDialog.Builder(MainActivity.this)
+//                        .setTitle("BMI說明")
+//                        .setMessage("體重(kg)/身高的平方(m)")
+//                        .setPositiveButton("OK", null)
+//                        .show();
+//            }
+//        });
         bHelp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                new AlertDialog.Builder(MainActivity.this)
-                        .setTitle("BMI說明")
-                        .setMessage("體重(kg)/身高的平方(m)")
-                        .setPositiveButton("OK", null)
-                        .show();
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("");
+                builder.setMessage("");
+                builder.setPositiveButton("OK",null);
+                builder.show();
             }
         });
+
+        bCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                bmi();
+                calBIM();
+            }
+        });
+
     }
 
-    public void bmi(View v){
+    private void calBIM() {
         String w = edWeight.getText().toString();
         String h = edHeight.getText().toString();
         float weight = Float.parseFloat(w);
         float height = Float.parseFloat(h);
         float bmi = weight/(height*height);
         Log.d("BMI", String.valueOf(bmi));
-        Intent intent = new Intent(this, ResultActivity.class);
+        Intent intent = new Intent(MainActivity.this, ResultActivity.class);
         intent.putExtra(getString(R.string.bmi_extra), bmi);
         startActivity(intent);
-        /*Toast.makeText(this, String.valueOf(bmi), Toast.LENGTH_LONG).show();
-        new AlertDialog.Builder(this)
-                .setMessage(bmi+"")
-                .setTitle("BMI運算")
-                .setPositiveButton("OK", null)
-                .show();*/
     }
+
+//    public void bmi(View v){
+//        String w = edWeight.getText().toString();
+//        String h = edHeight.getText().toString();
+//        float weight = Float.parseFloat(w);
+//        float height = Float.parseFloat(h);
+//        float bmi = weight/(height*height);
+//        Log.d("BMI", String.valueOf(bmi));
+//        Intent intent = new Intent(this, ResultActivity.class);
+//        intent.putExtra(getString(R.string.bmi_extra), bmi);
+//        startActivity(intent);
+//        /*Toast.makeText(this, String.valueOf(bmi), Toast.LENGTH_LONG).show();
+//        new AlertDialog.Builder(this)
+//                .setMessage(bmi+"")
+//                .setTitle("BMI運算")
+//                .setPositiveButton("OK", null)
+//                .show();*/
+//    }
 
     private void findViews() {
         //取得畫面元件
         edWeight = (EditText) findViewById(R.id.ed_weight);
         edHeight = (EditText) findViewById(R.id.ed_height);
         bHelp = (Button) findViewById(R.id.b_help);
+        bCalculate = (Button) findViewById(R.id.b_calculate);
     }
 
     @Override
